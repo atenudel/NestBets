7015ef1db4be424c8afa3860997ffcdd8ca34bd8
atenudel <aten@udel.edu>
2019-10-30 00:23:12 -0400

    TODO: fill in this api functions

--- src/controller.ts
+++ src/controller.ts
@@ -6,11 +6,47 @@ export class Controller {
     public welcomeMEssage(req: express.Request, res:express.Response): void {
         res.status(200).send(WELCOME_MESSAGE);
     }
-    
     public getHello(req: express.Request, res: express.Response): void {
         res.send("Hello World");
     }
     public postHello(req: express.Request, res: express.Response): void {
         res.send(req.body);
     }
+
+    public addUser(req: express.Request, res: express.Response): void {
+       
+    }
+
+    public getUser(req: express.Request, res: express.Response): void {
+       
+    }
+
+    public updateUser(req: express.Request, res: express.Response): void {
+       
+    }
+
+    public addBet(req: express.Request, res: express.Response): void {
+       
+    }
+
+    public getBet(req: express.Request, res: express.Response): void {
+       
+    }
+
+    public updateBet(req: express.Request, res: express.Response): void {
+       
+    }
+
+
+    public addMatch(req: express.Request, res: express.Response): void {
+       
+    }
+
+    public getMatch(req: express.Request, res: express.Response): void {
+       
+    }
+
+
+
+    
 }
