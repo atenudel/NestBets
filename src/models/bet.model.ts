6669c2b66c93993d50c66a3224b32126da4da045
atenudel <aten@udel.edu>
2019-10-30 00:02:38 -0400

    simple bet schema for now...

--- src/models/bet.model.ts
+++ src/models/bet.model.ts
@@ -1,8 +1,10 @@
 // this lets user add new bet
 import mongoose from 'mongoose';
+import { Decimal128 } from 'bson';
 // not ready to fill
 const BetSchema = new mongoose.Schema({
-
-
-
+    USERS: [String],
+    BETMONEY: [Decimal128]
 });
+
+export const bet = mongoose.model("bet",SpreadSchema);
