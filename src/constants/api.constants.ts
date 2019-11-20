64229d3e6afe10b64c95c26a46fbc985a96b1014
atenudel <aten@udel.edu>
2019-11-19 22:53:17 -0500

    stuff

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -1,7 +1,8 @@
 // api.constants.ts contains constants used throughout the api
 // default port for our app
 export const PORT = 3002;
+// you dont want to upload this...
 // using the ddefault mongodb port
-export const MONGO_URL = "mongodb://localhost:27017/football"
+export const MONGO_URL = "mongodb+srv://aten:paswordhere@cluster0-qjzex.mongodb.net/football?retryWrites=true&w=majority";
 
-export const WELCOME_MESSAGE = "Welcome to NestBets, We have the Best Bets!";
+export const WELCOME_MESSAGE = "Welcome!";
--- src/models/bet.model.ts
+++ src/models/bet.model.ts
@@ -1,10 +1,11 @@
 // this lets user add new bet
 import mongoose from 'mongoose';
-import { Decimal128, ObjectId } from 'bson';
+import {Decimal128, ObjectId } from 'bson';
+import {Spread} from "./spread.model"
 // not ready to fill
 const BetSchema = new mongoose.Schema({
   bet:{
-    MATCH: Number, //using id of the spread
+    MATCH: ObjectId, //using id of the spread
     USERS: [String], //using id of the users
     BETMONEY: [Number] // big hmmm
   } 
