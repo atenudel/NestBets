2b52f09e71dd580ed7aac26451016b206270e0a9
atenudel <aten@udel.edu>
2019-12-03 16:18:53 -0500

    whatever

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -3,6 +3,5 @@
 export const PORT = 3002;
 // you dont want to upload this...
 // using the ddefault mongodb port
-export const MONGO_URL = "mongodb+srv://@cluster0-qjzex.mongodb.net/football?retryWrites=true&w=majority";
 
 export const WELCOME_MESSAGE = "Welcome!";
